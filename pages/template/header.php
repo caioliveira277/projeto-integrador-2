b766c55c350ae332cc0a3111dcfe7bc4124b0ff7
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2020-10-20 20:32:50 -0300

    adicao header e footer completo

--- pages/template/header.php
+++ pages/template/header.php
@@ -4,7 +4,23 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./assets/css/styles.css">
   <link rel="shortcut icon" href="./assets/images/favicon.png" type="image/x-icon">
   <title>Document</title>
 </head>
 <body>
+  <header class="py-2">
+    <div class="container d-flex align-items-center justify-content-between">
+      <a href="./index.php">
+        <img src="./assets/images/logo.png" class="img-fluid" alt="Logo Portal dos games, um controle preto retrô com alguns botões em vermelho">
+      </a>
+      <nav>
+        <ul class="list-inline d-flex mb-0">
+          <li class="mx-1 d-flex"><a href="./index.php">Início</a></li>
+          <li class="mx-1 d-flex"><a href="./pages/quem_somos.php">Quem somos</a></li>
+          <li class="mx-1 d-flex"><a href="./pages/normas_utilizacao.php">Normas de utilização</a></li>
+          <li class="mx-1 d-flex"><a href="./pages/contato.php">Contato</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
