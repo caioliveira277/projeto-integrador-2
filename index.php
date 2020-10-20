3bac5d006e435bb47a7f9aa76789a4f2ff498560
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2020-10-20 19:06:38 -0300

    base do projeto

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include("./template/header.php"); ?>
+
+<main>
+  content  
+</main>
+
+<?php include("./template/footer.php"); ?>
---
+++ template/footer.php
@@ -0,0 +1,7 @@
+    <footer>
+      footer
+    </footer>
+    <script src="./assets/js/jquery-3.5.1.min.js"></script>
+    <script src="./assets/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
