e799a3ec992dfee5e54dba68d9114ac66fe8dec5
Juarez <juarezsouza.contato@hotmail.com>
2020-10-23 14:02:30 -0200

    acho_q_agora_vai_PAGINAS_RPG_E_PIXEL

---
+++ pages/pixel.php
@@ -0,0 +1,116 @@
+<main>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+
+                <h1 class="text-center">Conheça os jogos em Pixel que fazem sucesso no Brasil</h1>
+
+
+            </div>
+            <main>
+                <article>
+                    <div class="row">
+                        <div class="col-md-6 offset-md-3">
+                            <img class="img-fluid" src="./assets/images/celeste1.jpg" alt="Lenin" />
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lacinia semper aliquet.
+                                Nunc suscipit ullamcorper elit sit
+                                amet finibus. Ut et velit fringilla magna tempus ultrices facilisis et leo. Duis a metus
+                                eu justo accumsan auctor. Sed eu
+                                odio magna. Suspendisse enim dolor, consectetur quis ullamcorper sed, imperdiet non ex.
+                                Proin eros dolor, dictum vel lacinia
+                                eu, commodo quis sapien. Suspendisse laoreet leo vel lectus gravida, non faucibus velit
+                                euismod. Cras vulputate pulvinar
+                                porttitor. Vivamus vitae tempor nisi. Sed fermentum sapien augue, sodales blandit mauris
+                                porttitor nec. Etiam efficitur odio id
+                                imperdiet sollicitudin. Donec auctor dui id lacus tempor, et aliquet risus viverra.
+                                Pellentesque dolor lectus, lobortis id ante
+                                condimentum, tincidunt elementum risus. Duis nec sollicitudin nisi. Nunc eget
+                                pellentesque purus. Donec sed condimentum ipsum.
+                                Phasellus non vulputate magna. Vestibulum nec dignissim tortor. Nulla ultricies, orci et
+                                blandit fermentum, metus dolor interdum
+                                lacus, eget luctus nisi felis et quam. Fusce egestas euismod erat sit amet eleifend.
+                                Donec porta, nulla id ullamcorper maximus,
+                                magna arcu tincidunt nisl, ac ultricies mi risus ornare ex. Sed purus ligula, fringilla
+                                a diam vitae, aliquam gravida nisi.
+                                Vivamus accumsan iaculis turpis, id elementum erat suscipit ac. Nulla non tempor massa.
+                            </p>
+                        </div>
+                    </div>
+                </article>
+                <section>
+                    <div class="row">
+                        <div class="col-md-6 offset-md-3">
+
+                            <h2>Veja agora alguns games brasileiros de RPG</h2>
+
+
+                            <p class="text-primary"><b>Celeste</b></p>
+                            <img class="img-fluid" src="./assets/images/celeste_inicio.png" alt="Lenin" />
+
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lacinia semper aliquet.
+                                Nunc suscipit ullamcorper elit sit
+                                amet finibus. Ut et velit fringilla magna tempus ultrices facilisis et leo. Duis a metus
+                                eu justo accumsan auctor. Sed eu
+                                odio magna. Suspendisse enim dolor, consectetur quis ullamcorper sed, imperdiet non ex.
+                                Proin eros dolor, dictum vel lacinia
+                                eu, commodo quis sapien. Suspendisse laoreet leo vel lectus gravida, non faucibus velit
+                                euismod. Cras vulputate pulvinar
+                                porttitor. Vivamus vitae tempor nisi. Sed fermentum sapien augue, sodales blandit mauris
+                                porttitor nec. Etiam efficitur odio id
+                                imperdiet sollicitudin. Donec auctor dui id lacus tempor, et aliquet risus viverra.
+                                Pellentesque dolor lectus, lobortis id ante
+                                condimentum, tincidunt elementum risus. Duis nec sollicitudin nisi. Nunc eget
+                                pellentesque purus. Donec sed condimentum ipsum.
+                                Phasellus non vulputate magna. Vestibulum nec dignissim tortor. Nulla ultricies, orci et
+                                blandit fermentum, metus dolor interdum
+                                lacus, eget luctus nisi felis et quam. Fusce egestas euismod erat sit amet eleifend.
+                                Donec porta, nulla id ullamcorper maximus,
+                                magna arcu tincidunt nisl, ac ultricies mi risus ornare ex. Sed purus ligula, fringilla
+                                a diam vitae, aliquam gravida nisi.
+                                Vivamus accumsan iaculis turpis, id elementum erat suscipit ac. Nulla non tempor massa.
+                            </p>
+
+                        </div>
+                    </div>
+                </section>
+
+                <section>
+                    <div class="row">
+                        <div class="col-md-6 offset-md-3">
+
+                            <p class="text-primary"><b>Chroma Squad (2015)</b></p>
+                            <img class="img-fluid" src="assets/images/chroma.jpg" alt="Lenin" />
+
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lacinia semper aliquet.
+                                Nunc suscipit ullamcorper elit sit
+                                amet finibus. Ut et velit fringilla magna tempus ultrices facilisis et leo. Duis a metus
+                                eu justo accumsan auctor. Sed eu
+                                odio magna. Suspendisse enim dolor, consectetur quis ullamcorper sed, imperdiet non ex.
+                                Proin eros dolor, dictum vel lacinia
+                                eu, commodo quis sapien. Suspendisse laoreet leo vel lectus gravida, non faucibus velit
+                                euismod. Cras vulputate pulvinar
+                                porttitor. Vivamus vitae tempor nisi. Sed fermentum sapien augue, sodales blandit mauris
+                                porttitor nec. Etiam efficitur odio id
+                                imperdiet sollicitudin. Donec auctor dui id lacus tempor, et aliquet risus viverra.
+                                Pellentesque dolor lectus, lobortis id ante
+                                condimentum, tincidunt elementum risus. Duis nec sollicitudin nisi. Nunc eget
+                                pellentesque purus. Donec sed condimentum ipsum.
+                                Phasellus non vulputate magna. Vestibulum nec dignissim tortor. Nulla ultricies, orci et
+                                blandit fermentum, metus dolor interdum
+                                lacus, eget luctus nisi felis et quam. Fusce egestas euismod erat sit amet eleifend.
+                                Donec porta, nulla id ullamcorper maximus,
+                                magna arcu tincidunt nisl, ac ultricies mi risus ornare ex. Sed purus ligula, fringilla
+                                a diam vitae, aliquam gravida nisi.
+                                Vivamus accumsan iaculis turpis, id elementum erat suscipit ac. Nulla non tempor massa.
+                            </p>
+
+                        </div>
+                    </div>
+                </section>
+            </main>
+
+        </div>
+
+    </div>
+
+</main>
