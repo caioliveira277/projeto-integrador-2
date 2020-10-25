d898cdbb454586f1100f1228e5d65fe20db07439
Leonardo Bozzi <bozzileonardo8@gmail.com>
2020-10-25 20:18:22 -0300

    agr ta top dms

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
-<?php 
-  $view = filter_input(INPUT_GET, "view");
+<?php
+$view = filter_input(INPUT_GET, "view");
 
-  include("./pages/template/header.php");
-    switch ($view) {
-      case 'acao_aventura':
-        include("./pages/conteudo_acao_aventura.php");
-        break;
+include("./pages/template/header.php");
+switch ($view) {
+  case 'acao_aventura':
+    include("./pages/conteudo_acao_aventura.php");
+    break;
 
-        case 'contato':
-          include("./pages/contato.php");
-          break;
-      
-      default:
-        include("./pages/home.php");
-        break;
-    }
-  include("./pages/template/footer.php"); 
-?>
+  case 'contato':
+    include("./pages/contato.php");
+    break;
+  case 'home':
+    include("./pages/home.php");
+    break;
+  default:
+    include("./pages/home.php");
+    break;
+}
+include("./pages/template/footer.php");
