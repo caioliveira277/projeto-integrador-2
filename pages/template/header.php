164bc229da7fa8672b41469535194e94ba8bbeb9
Leonardo Bozzi <bozzileonardo8@gmail.com>
2020-10-25 21:58:44 -0300

    Header responsivo

--- pages/template/header.php
+++ pages/template/header.php
@@ -1,26 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
   <link rel="stylesheet" href="./assets/css/styles.css">
   <link rel="shortcut icon" href="./assets/images/favicon.png" type="image/x-icon">
-  <title>Document</title>
+  <title>Portal dos Games</title>
 </head>
+
 <body>
-  <header class="py-2 mb-5">
-    <div class="container d-flex align-items-center justify-content-between">
-      <a href="./index.php">
-        <img src="./assets/images/logo.png" class="img-fluid" alt="Logo Portal dos games, um controle preto retrô com alguns botões em vermelho">
+  <header>
+    <nav class="navbar navbar-expand-md py-3">
+      <a href="index.html" class="navbar-brand">
+        <img src="assets/images/logo.png" alt="Portal dos Games">
       </a>
-      <nav>
-        <ul class="list-inline d-flex mb-0">
-          <li class="mx-1 d-flex"><a href="./index.php">Início</a></li>
-          <li class="mx-1 d-flex"><a href="./pages/quem_somos.php">Quem somos</a></li>
-          <li class="mx-1 d-flex"><a href="./pages/normas_utilizacao.php">Normas de utilização</a></li>
-          <li class="mx-1 d-flex"><a href="./pages/contato.php">Contato</a></li>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Abrir Navegação">
+        <span class="navbar-toggler-icon m-3 d-flex justify-content-center align-items-center"><img src="assets/images/menu.png"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="mx-1 d-flex">
+            <a class="nav-link" href="./index.php">Início</a>
+          </li>
+          <li class="mx-1 d-flex">
+            <a class="nav-link active" href="./pages/quem_somos.php">Quem somos</a>
+          </li>
+          <li class="mx-1 d-flex">
+            <a class="nav-link active" href="./pages/normas_utilizacao.php">Normas de Utilização</a>
+          </li>
+          <li class="mx-1 d-flex">
+            <a class="nav-link active" href="./pages/contato.php">Contato</a>
+          </li>
         </ul>
-      </nav>
-    </div>
+      </div>
+    </nav>
   </header>
