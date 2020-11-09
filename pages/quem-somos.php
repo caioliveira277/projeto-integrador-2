a7d1498222d06b936d965f5a07302241e2a2a486
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2020-11-09 20:52:04 -0300

    finalizacao quem somos

--- pages/quem-somos.php
+++ pages/quem-somos.php
@@ -1,4 +1,4 @@
-<div class="container">
+<main class="container quem-somos">
     <div class="row">
         <div class="col-12">
             <h3>Quem Somos</h3>
@@ -6,29 +6,29 @@
     </div>
     <div class="row">
         <div class="col-lg-6 col-md-12 col-sm-12 p-3 d-flex justify-content-center align-items-center">
-            <div class="card d-flex justify-content-center align-items-center p-3" style="width: 35rem;">
-                <img src="assets/images/alex_lg.png" class="card-img-top" alt="Alex Teixeira da Fonseca" style="max-width: 340px;">
+            <div class="card d-flex justify-content-center align-items-center p-3">
+                <img src="assets/images/alex_lg.png" alt="Foto de Alex Teixeira da Fonseca">
                 <div class="card-body text-center">
                     <h4>Alex Teixeira</h4>
                     <div class="d-flex justify-content-between px-5">
                         <p>Função: Designer</p>
                         <p>R.A: 2650832013009</p>
                     </div>
-                    <p>Página feita: Quem Somos</p>
+                    <p>Página feita: Contato e Normas de utilização</p>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
             </div>
         </div>
         <div class="col-lg-6 col-md-12 col-sm-12 p-3 d-flex justify-content-center align-items-center">
-            <div class="card d-flex justify-content-center align-items-center p-3" style="width: 35rem;">
-                <img src="assets/images/quem_somos.png" class="card-img-top" alt="Guilherme Moreira dos Santos" style="max-width: 340px;">
+            <div class="card d-flex justify-content-center align-items-center p-3">
+                <img src="assets/images/guilherme_lg.png" alt="Foto de Guilherme Moreira dos Santos">
                 <div class="card-body text-center">
                     <h4>Guilherme Moreira</h4>
                     <div class="d-flex justify-content-between px-5">
                         <p>Função: Designer</p>
                         <p>R.A: 2650832013001</p>
                     </div>
-                    <p>Página feita: Quem Somos</p>
+                    <p>Página feita: Terror</p>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
             </div>
@@ -36,15 +36,15 @@
     </div>
     <div class="row">
         <div class="col-lg-12 col-md-12 col-sm-12 p-3 d-flex justify-content-center align-items-center">
-            <div class="card d-flex justify-content-center align-items-center p-3" style="width: 35rem;">
-                <img src="assets/images/quem_somos.png" class="card-img-top" alt="Caio Henrique da Silva Oliveira" style="max-width: 340px;">
+            <div class="card d-flex justify-content-center align-items-center p-3">
+                <img src="assets/images/caio_lg.png" alt="Foto de Caio Henrique da Silva Oliveira">
                 <div class="card-body text-center">
                     <h4>Caio Oliveira</h4>
                     <div class="d-flex justify-content-between px-5">
-                        <p>Função: Gerente</p>
+                        <p>Função: Cordenador</p>
                         <p>R.A: 2650832013013</p>
                     </div>
-                    <p>Página feita: Quem Somos</p>
+                    <p>Cuidou: Estrutura</p>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
             </div>
@@ -52,32 +52,32 @@
     </div>
     <div class="row">
         <div class="col-lg-6 col-md-12 col-sm-12 p-3 d-flex justify-content-center align-items-center">
-            <div class="card d-flex justify-content-center align-items-center p-3" style="width: 35rem;">
-                <img src="assets/images/quem_somos.png" class="card-img-top" alt="Leonardo Bozzi da Silva" style="max-width: 340px;">
+            <div class="card d-flex justify-content-center align-items-center p-3">
+                <img src="assets/images/leonardo_lg.png" alt="Foto de Leonardo Bozzi da Silva">
                 <div class="card-body text-center">
                     <h4>Leonardo Bozzi</h4>
                     <div class="d-flex justify-content-between px-5">
                         <p>Função: Programador</p>
                         <p>R.A: 2650832013008</p>
                     </div>
-                    <p>Página feita: Quem Somos</p>
+                    <p>Página feita: Home e Quem Somos</p>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
             </div>
         </div>
         <div class="col-lg-6 col-md-12 col-sm-12 p-3 d-flex justify-content-center align-items-center">
-            <div class="card d-flex justify-content-center align-items-center p-3" style="width: 35rem;">
-                <img src="assets/images/quem_somos.png" class="card-img-top" alt="Guilherme Moreira dos Santos" style="max-width: 340px;">
+            <div class="card d-flex justify-content-center align-items-center p-3">
+                <img src="assets/images/juarez_lg.png" alt="Foto de Juarez Caetano">
                 <div class="card-body text-center">
                     <h4>Juarez Caetano</h4>
                     <div class="d-flex justify-content-between px-5">
                         <p>Função: Programador</p>
                         <p>R.A: 2650832013004</p>
                     </div>
-                    <p>Página feita: Quem Somos</p>
+                    <p>Página feita: RPG e Pixel</p>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</main>
