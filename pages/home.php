e3d75078b7a8d7c6b389b3f41aa902f92f49eec4
xeon <xeon@Xeon.Xeon>
2020-11-22 12:08:16 -0300

    home completa

--- pages/home.php
+++ pages/home.php
@@ -1,21 +1,15 @@
-<main class="pagina-home container mt-5">
-    <div class="row d-flex justify-content-center">
-        <div class="col-lg-8 text-center">
-            <h1 class="title-home">Jogos que são lançados aqui no Brasil mas não temos a menor ideia. </h1>
-        </div>
-    </div>
-    <div class="row autor-postagem pt-5 mt-3">
-        <figure class="col-lg-6 d-flex justify-content-md-start justify-content-sm-center">
+<main class="pagina-home container-md">
+    <h1 class="font-weight-bold my-5">Jogos que são lançados aqui no Brasil mas não temos a menor ideia. </h1>
+    <section class="row mb-3 align-items-center">
+        <figure class="d-flex mb-0 col-6">
             <img src="./assets/images/alex_sm.png" class="rounded-circle img-perfil mr-3" alt="Imagem de perfil do autor da postagem">
-            <figcaption>
-                <div class="d-flex">
-                    <p class="font-weight-normal mb-0 nome">Alex Teixeira</p>
-                    <a href="#" class="btn btn-success bg-transparent text-success ml-3 p-1" target="_blank">Seguir</a>
-                </div>
-                <p class="font-weight-light mb-0 mt-1">22 de setembro de 2020</p>
+            <figcaption class="row align-items-center">
+                <p class="font-weight-light mb-0 col-auto">Alex Teixeira</p>
+                <a href="#" class="btn btn-success bg-transparent text-success ml-3 p-1 col-auto" target="_blank">Seguir</a>
+                <p class="font-weight-light mb-0 mt-2 col-12">22 de setembro de 2020</p>
             </figcaption>
         </figure>
-        <ul class="list-inline d-flex mb-0 col-lg-6 p-sm-5 align-items-center justify-content-end justify-content-sm-center">
+        <ul class="list-inline d-flex col-6 mb-0 justify-content-end">
             <li class="mx-3">
                 <a href="https://www.facebook.com/" target="_blank">
                     <img src="./assets/icons/facebook-black.svg" alt="Logomarca Facebook">
@@ -37,120 +31,106 @@
                 </a>
             </li>
         </ul>
-    </div>
-    <div class="row">
-        <div class="col-12">
-            <img class="img-fluid" src="./assets/images/banner_home.jpg" alt="Um homem utilizando seu computador">
-        </div>
-    </div>
-    <div class="row">
-        <div class="modal fade" id="modalExemplo" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Comentários</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <p>POST SHOW ALEX!</p>
-                        <small class="text-comment-name">MrFall</small>
-                        <hr>
-                        <p>PARABENS!!!</p>
-                        <small class="text-comment-name">shaolinmatadordeporco</small>
-                        <hr>
-                        <p>SURPREENDENTE!!!</p>
-                        <small class="text-comment-name">viniccius13</small>
-                        <hr>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <img class="img-fluid mt-3" src="./assets/images/banner_home.jpg" alt="Um homem utilizando seu computador">
         <div class="col-3 d-flex align-items-center reacoes p-4">
             <img class="m-1" src="./assets/icons/like.svg" alt="Icone de joia representando 'gostei'">
-            <p class="m-1">12k</p>
+            <span class="m-1">12k</span>
             <img class="m-1" data-toggle="modal" data-target="#modalExemplo" src="./assets/icons/comentarios.svg" alt="Icone de chat representando 'número de comentários'">
-            <p class="m-1">39</p>
+            <span class="m-1">39</span>
             <img class="m-1" src="./assets/icons/save.svg" alt="Icone de coração representando 'salvar'">
-            <p class="m-1">Salvar</p>
+            <span class="m-1">Salvar</span>
         </div>
-    </div>
-    <div class="row d-flex justify-content-center align-items-center">
-        <div class="col-12 col-lg-9">
-            <p class="main-text-home">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lacinia semper aliquet. Nunc suscipit ullamcorper elit sit amet finibus. Ut et velit fringilla magna tempus ultrices facilisis et leo. Duis a metus eu justo accumsan auctor. Sed eu odio magna. Suspendisse enim dolor, consectetur quis ullamcorper sed, imperdiet non ex. Proin eros dolor, dictum vel lacinia eu, commodo quis sapien. Suspendisse laoreet leo vel lectus gravida, non faucibus velit euismod. Cras vulputate pulvinar porttitor. Vivamus vitae tempor nisi. Sed fermentum sapien augue, sodales blandit mauris porttitor nec. Etiam efficitur odio id imperdiet sollicitudin. Donec auctor dui id lacus tempor, et aliquet risus viverra. Pellentesque dolor lectus, lobortis id ante condimentum, tincidunt elementum risus.
-            </p>
-
-            <p class="main-text-home">Duis nec sollicitudin nisi. Nunc eget pellentesque purus. Donec sed condimentum ipsum. Phasellus non vulputate magna. Vestibulum nec dignissim tortor. Nulla ultricies, orci et blandit fermentum, metus dolor interdum lacus, eget luctus nisi felis et quam. Fusce egestas euismod erat sit amet eleifend. Donec porta, nulla id ullamcorper maximus, magna arcu tincidunt nisl, ac ultricies mi risus ornare ex. Sed purus ligula, fringilla a diam vitae, aliquam gravida nisi. Vivamus accumsan iaculis turpis, id elementum erat suscipit ac. Nulla non tempor massa.</p>
-        </div>
-    </div>
-    <div class="row d-flex justify-content-center align-items-center">
-        <div class="col-lg-4 col-md-9 text-center">
-            <img src="./assets/images/artigo-home.png" class="img-fluid mb-2" alt="Uma mesa de madeira com um teclado preto, um celular, notebook, caixa de som e um monitor">
-        </div>
-        <div class="col-lg-5 col-md-9">
-            <p class="main-text-home">Duis nec sollicitudin nisi. Nunc eget pellentesque purus. Donec sed condimentum ipsum. Phasellus non vulputate magna. Vestibulum nec dignissim tortor. Nulla ultricies, orci et blandit fermentum, metus dolor interdum lacus, eget luctus nisi felis et quam. Fusce egestas euismod erat sit amet eleifend. Donec porta, nulla id ullamcorper maximus, magna arcu tincidunt nisl, ac ultricies mi risus ornare ex. Sed purus ligula, fringilla a diam vitae, aliquam gravida nisi. Vivamus accumsan iaculis turpis, id elementum erat suscipit ac. Nulla non tempor massa.</p>
-        </div>
-    </div>
-    <div class="row m-2 mt-4 m-lg-5 d-flex justify-content-center align-items-center">
-        <div class="col-12 col-lg-6 text-center">
-            <h3>Conheça os estilos de jogos que são lançados aqui no Brasil e as empresas que os criaram.</h3>
-            <hr>
-        </div>
-    </div>
-    <div class="row m-lg-4">
-        <div class="col-lg-6 col-md-12 text-center games-home">
-            <div class="terror">
-                <a href="./index.php?view=terror"><img class="img-fluid m-lg-2 mb-4 mb-lg-0" src="./assets/images/terror.png" alt="Ilustração de um cartão com desenhos sobre jogos de terror"></a>
+    </section>
+    <section class="row justify-content-center">
+        <p class="paragrafo col-11">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lacinia semper aliquet. Nunc suscipit ullamcorper elit sit amet finibus. Ut et velit fringilla magna tempus ultrices facilisis et leo. Duis a metus eu justo accumsan auctor. Sed eu odio magna. Suspendisse enim dolor, consectetur quis ullamcorper sed, imperdiet non ex. Proin eros dolor, dictum vel lacinia eu, commodo quis sapien. Suspendisse laoreet leo vel lectus gravida, non faucibus velit euismod. Cras vulputate pulvinar porttitor. Vivamus vitae tempor nisi. Sed fermentum sapien augue, sodales blandit mauris porttitor nec. Etiam efficitur odio id imperdiet sollicitudin. Donec auctor dui id lacus tempor, et aliquet risus viverra. Pellentesque dolor lectus, lobortis id ante condimentum, tincidunt elementum risus.
+        </p>
+        <p class="paragrafo col-11">Duis nec sollicitudin nisi. Nunc eget pellentesque purus. Donec sed condimentum ipsum. Phasellus non vulputate magna. Vestibulum nec dignissim tortor. Nulla ultricies, orci et blandit fermentum, metus dolor interdum lacus, eget luctus nisi felis et quam. Fusce egestas euismod erat sit amet eleifend. Donec porta, nulla id ullamcorper maximus, magna arcu tincidunt nisl, ac ultricies mi risus ornare ex. Sed purus ligula, fringilla a diam vitae, aliquam gravida nisi. Vivamus accumsan iaculis turpis, id elementum erat suscipit ac. Nulla non tempor massa.</p>
+        <figure class="col-12 col-md-4 m-0">
+            <img src="./assets/images/artigo-home.png" class="d-none d-md-block img-fluid" alt="Uma mesa de madeira com um teclado preto, um celular, notebook, caixa de som e um monitor">
+            <img src="./assets/images/artigo-home-md.png" class="d-block d-md-none img-fluid" alt="Uma mesa de madeira com um teclado preto, um celular, notebook, caixa de som e um monitor">
+        </figure>
+        <p class="paragrafo col-12 col-md-8">Duis nec sollicitudin nisi. Nunc eget pellentesque purus. Donec sed condimentum ipsum. Phasellus non vulputate magna. Vestibulum nec dignissim tortor. Nulla ultricies, orci et blandit fermentum, metus dolor interdum lacus, eget luctus nisi felis et quam. Fusce egestas euismod erat sit amet eleifend. Donec porta, nulla id ullamcorper maximus, magna arcu tincidunt nisl, ac ultricies mi risus ornare ex. Sed purus ligula, fringilla a diam vitae, aliquam gravida nisi. Vivamus accumsan iaculis turpis, id elementum erat suscipit ac. Nulla non tempor massa.</p>
+    </section>
+    <section class="my-5">
+        <h2 class="px-2">Conheça os estilos de jogos que são lançados <br> aqui no Brasil e as empresas que os criaram.</h2>
+        <hr class="mb-5">
+        <div class="row">
+            <div class="col-12 col-md-6 mb-4 position-relative">
+                <img class="img-fluid" src="./assets/images/terror.png" alt="Ilustração de um cartão com desenhos sobre jogos de terror">
+                <a href="./index.php?view=terror" class="btn btn-outline-light px-4 position-absolute btn-conheca">
+                    Conheça
+                </a>
+            </div>
+            <div class="col-12 col-md-6 mb-4 position-relative">
+                <img class="img-fluid" src="./assets/images/acao-aventura.png">
+                <a href="./index.php?view=acao-aventura" class="btn btn-outline-light px-4 position-absolute btn-conheca">
+                    Conheça
+                </a>
+            </div>
+            <div class="col-12 col-md-6 mt-1 mb-4 position-relative">
+                <img class="img-fluid" src="./assets/images/rpg.png" alt="Ilustração de um cartão com desenhos sobre jogos de RPG">
+                <a href="./index.php?view=rpg" class="btn btn-outline-light px-4 position-absolute btn-conheca">
+                    Conheça
+                </a>
+            </div>
+            <div class="col-12 col-md-6 mt-1 mb-4 position-relative">
+                <img class="img-fluid" src="./assets/images/pixel.png" alt="Ilustração de um cartão com desenhos sobre jogos feitos em Pixels">
+                <a href="./index.php?view=pixel" class="btn btn-outline-light px-4 position-absolute btn-conheca">
+                    Conheça
+                </a>
             </div>
         </div>
-        <div class="col-lg-6 col-md-12 text-center games-home">
-            <div class="acao">
-                <a href="./index.php?view=acao-aventura"><img class="img-fluid m-lg-2 mb-4 mb-lg-0" src="./assets/images/acao.png"></a>
-            </div>
-        </div>
-    </div>
-    <div class="row m-lg-4 games-home">
-        <div class="col-lg-6 col-md-12 text-center games-home">
-            <div class="rpg">
-                <a href="./index.php?view=rpg"><img class="img-fluid m-lg-2 mb-4 mb-lg-0" src="./assets/images/rpg.png" alt="Ilustração de um cartão com desenhos sobre jogos de RPG"></a>
-            </div>
-        </div>
-        <div class="col-lg-6 col-md-12 text-center games-home">
-            <div class="pixel">
-                <a href="./index.php?view=pixel"><img class="img-fluid m-lg-2 mb-4 mb-lg-0" src="./assets/images/pixel.png" alt="Ilustração de um cartão com desenhos sobre jogos feitos em Pixels"></a>
-            </div>
-        </div>
-    </div>
-    <div class="row d-flex justify-content-center align-items-center text-center title-email-home mt-4">
-        <h1>Receba as notícias na hora em que elas acontecem</h1>
-    </div>
-    <div class="row d-flex justify-content-center align-items-center form-email-contact">
-        <div class="col-lg-5 col-md-8 col-sm-6">
+    </section>
+    <section class="row justify-content-center align-items-center form-email-contact">
+        <h5 class="col-12 text-center my-4">Receba as notícias na hora em que elas acontecem</h5>
+        <div class="col-12 col-md-6">
             <form class="p-2 form-home">
                 <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Digite seu nome completo ">
+                    <label for="nome" class="font-weight-light">Nome:</label>
+                    <input type="text" id="nome" class="form-control py-1" placeholder="Digite seu nome completo ">
                 </div>
                 <div class="form-group">
-                    <input type="email" class="form-control" placeholder="teste@email.com">
+                    <label for="email" class="font-weight-light">Email:</label>
+                    <input type="email" id="email" class="form-control py-1" placeholder="teste@email.com">
                 </div>
             </form>
         </div>
-        <div class="col-lg-4 col-md-8 col-sm-6 p-2 text-center">
-            <p>
+        <div class="col-10 col-md-6 col-lg-4 p-2">
+            <p class="font-weight-light text-center">
                 Te ajudamos com as notícias essenciais para começar o seu dia da melhor forma possivel.
             </p>
-            <label for="send" class="btn btn-primary p-0 d-flex justify-content-center align-items-center button-send-mail">
-                <span class="m-2">
-                    <img id="sendmail" src="./assets/icons/sendmail.svg" alt="Icone de carta representando o envio de uma mensagem">
-                </span>
+            <a href="#" class="btn btn-primary p-0 d-flex justify-content-center align-items-center button-send-mail">
+                <img id="sendmail" class="m-2" src="./assets/icons/sendmail.svg" alt="Icone de carta representando o envio de uma mensagem">
                 Aceito Receber Novidades
-            </label>
-            <small>Você receberá um e-mail de confirmação em seguida.</small>
+            </a>
+            <small class="font-weight-light">Você receberá um e-mail de confirmação em seguida.</small>
+        </div>
+    </section>
+</main>
+<div class="modal fade" id="modalExemplo" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Comentários</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>POST SHOW ALEX!</p>
+                <small class="text-comment-name">MrFall</small>
+                <hr>
+                <p>PARABENS!!!</p>
+                <small class="text-comment-name">shaolinmatadordeporco</small>
+                <hr>
+                <p>SURPREENDENTE!!!</p>
+                <small class="text-comment-name">viniccius13</small>
+                <hr>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+            </div>
         </div>
     </div>
-</main>
+</div>
