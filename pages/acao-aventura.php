68560d0d19562e503dfd4a79d6b35bcd868fb7c6
xeon <xeon@Xeon.Xeon>
2020-11-22 15:55:25 -0300

    pagina acao finalizada

--- pages/acao-aventura.php
+++ pages/acao-aventura.php
@@ -1,31 +1,35 @@
-<main class="container acao-aventura">
-    <div class="d-flex flex-column justify-content-center banner-postagem mt-2 px-3">
-        <em class="text-center m-auto">
-            <h1>Jogos de Ação e aventura</h1>
-            <h2 class="mt-4">Grandes clássicos para reunir o squad e partir para cima </h2>
-        </em>
-        <div class="d-flex justify-content-around flex-column flex-md-row mb-5">
-            <a href="#" class="btn btn-outline-warning rounded-pill px-5 my-2 my-md-0 font-weight-bold">RPG</a>    
-            <a href="#" class="btn btn-outline-warning rounded-pill px-5 my-2 my-md-0 font-weight-bold">TERROR</a>    
-            <a href="#" class="btn btn-outline-warning rounded-pill px-5 my-2 my-md-0 font-weight-bold">PIXEL</a>    
+<main class="acao-aventura bg-preto pb-5">
+    <section class="container-xl px-0 px-lg-2">
+        <div class="d-flex flex-column justify-content-center banner-postagem px-3">
+            <em class="text-center m-auto">
+                <h1 class="titulo-sombreado">Jogos de Ação e aventura</h1>
+                <h2 class="mt-4 subtitulo-opacidade">Grandes clássicos para reunir o squad e partir para cima </h2>
+            </em>
+            <div class="row justify-content-around mb-5 flex-column flex-md-row align-items-center align-items-md-center">
+                <a href="#" class="btn btn-outline-warning rounded-pill px-5 my-2 my-md-0 col-12 col-md-4 font-weight-bold w-customizado">RPG</a>    
+                <a href="#" class="btn btn-outline-warning rounded-pill px-5 my-2 my-md-0 col-12 col-md-4 font-weight-bold w-customizado">TERROR</a>    
+                <a href="#" class="btn btn-outline-warning rounded-pill px-5 my-2 my-md-0 col-12 col-md-4 font-weight-bold w-customizado">PIXEL</a>    
+            </div>
         </div>
-    </div>
-    <article class="position-relative banner-1 mt-5">
-        <span class="numero-artigo text-warning">01</span>
-        <div class="ml-lg-auto col-12 col-lg-6 bg-opacidade p-lg-4">
-            <h2 class="text-warning text-left text-lg-right ml-1 mr-lg-5 pt-3">171</h2>
-            <p class="text-white bg-opacidade px-lg-5 pt-4 pb-5">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ullamcorper consectetur velit. Curabitur in vestibulum magna. Nullam porttitor sapien lobortis, sagittis diam eget, luctus lorem. Maecenas mauris nulla, ullamcorper ac varius vel, efficitur a sapien. Pellentesque non laoreet felis. Integer elementum in libero id tempor. Curabitur eu ex in lorem vestibulum suscipit. In volutpat feugiat dui, at tempor erat eleifend convallis. In ut leo eu sapien porttitor interdum suscipit id mi.
-            </p>
-        </div>
-    </article>
-    <article class="position-relative banner-2 mt-5">
-        <span class="numero-artigo text-warning">02</span>
-        <div class="ml-lg-auto col-12 col-lg-6 bg-opacidade p-lg-4">
-            <h2 class="text-warning text-left text-lg-right ml-1 mr-lg-5 pt-3">RIO</h2>
-            <p class="text-white bg-opacidade px-lg-5 pt-4 pb-5">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ullamcorper consectetur velit. Curabitur in vestibulum magna. Nullam porttitor sapien lobortis, sagittis diam eget, luctus lorem. Maecenas mauris nulla, ullamcorper ac varius vel, efficitur a sapien. Pellentesque non laoreet felis. Integer elementum in libero id tempor. Curabitur eu ex in lorem vestibulum suscipit. In volutpat feugiat dui, at tempor erat eleifend convallis. In ut leo eu sapien porttitor interdum suscipit id mi.
-            </p>
-        </div>
-    </article>
+        <article class="position-relative banner-1 mt-5">
+            <span class="numero-artigo text-warning">01</span>
+            <div class="ml-sm-auto col-12 col-sm-9 col-lg-6 bg-opacidade px-0 p-lg-4">
+                <img src="./assets/images/171-sm.png" class="d-block img-fluid d-sm-none" alt="Sena do jogo 171, um personagem atirando contra um carro em meio a rua">
+                <h2 class="text-warning text-left text-right ml-1 mr-3 mr-sm-5 pt-3">171</h2>
+                <p class="text-white bg-opacidade px-3 pl-sm-5 pt-4 pb-5 paragrafo">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ullamcorper consectetur velit. Curabitur in vestibulum magna. Nullam porttitor sapien lobortis, sagittis diam eget, luctus lorem. Maecenas mauris nulla, ullamcorper ac varius vel, efficitur a sapien. Pellentesque non laoreet felis. Integer elementum in libero id tempor. Curabitur eu ex in lorem vestibulum suscipit. In volutpat feugiat dui, at tempor erat eleifend convallis. In ut leo eu sapien porttitor interdum suscipit id mi.
+                </p>
+            </div>
+        </article>
+        <article class="position-relative banner-2 mt-5 mb-4">
+            <span class="numero-artigo text-warning">02</span>
+            <div class="ml-sm-auto col-12 col-sm-9 col-lg-6 bg-opacidade px-0 p-lg-4">
+                <img src="./assets/images/rio-sm.png" class="d-block img-fluid d-sm-none" alt="Sena do jogo Rio, um personagem em frente a uma pedra com uma frase escrita">
+                <h2 class="text-warning text-left text-right ml-1 mr-3 mr-sm-5 pt-3">RIO</h2>
+                <p class="text-white bg-opacidade px-3 pl-sm-5 pt-4 pb-5 paragrafo">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ullamcorper consectetur velit. Curabitur in vestibulum magna. Nullam porttitor sapien lobortis, sagittis diam eget, luctus lorem. Maecenas mauris nulla, ullamcorper ac varius vel, efficitur a sapien. Pellentesque non laoreet felis. Integer elementum in libero id tempor. Curabitur eu ex in lorem vestibulum suscipit. In volutpat feugiat dui, at tempor erat eleifend convallis. In ut leo eu sapien porttitor interdum suscipit id mi.
+                </p>
+            </div>
+        </article>
+    </section>
 </main>
